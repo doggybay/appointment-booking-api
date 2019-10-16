de63487155cafef3b97ee39c9f451011f0a5bd86
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-15 20:17:36 -0700

    first commit

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+let connectionString = process.platform === 'win32' ? 'postgres://db_access:freedom@localhost/appt-booking-app' : 'postgres://localhost/appt-booking-app'
+  
+  module.exports = {
+    development: {
+        client: 'pg',
+        connection: connectionString,
+        migrations: {
+            directory: __dirname + '/db/migrations',
+          },
+        seeds: {
+            directory: __dirname + '/db/seeds',
+          },
+      },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: __dirname + '/db/migrations',
+          },
+        seeds: {
+            directory: __dirname + '/db/seeds',
+          },
+      },
+  };
+  
