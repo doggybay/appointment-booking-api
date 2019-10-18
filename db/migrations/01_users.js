9f894d5abae68727379cd1e875535cedee6e45e0
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-17 19:16:51 -0700

    working on tables

--- db/migrations/01_users.js
+++ db/migrations/01_users.js
@@ -2,10 +2,12 @@
 exports.up = function(knex) {
   return knex.schema.createTable("users", function(table) {
     table.increments();
-    table.string("name", 255).notNullable();
-    table.string("email", 255).notNullable;
-    table.string("password", 255).notNullable();
-    table.string("created_at").notNullable();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("email").notNullable();
+    table.string("password").notNullable();
+    table.string("phone").notNullable();
+    table.timestamps(true, true).notNullable();
   })
 };
 
--- db/migrations/02_appointments.js
+++ db/migrations/02_appointments.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("appointments", function (table) {
+    table.increments();
+    table.string("title").notNullable();
+    table.text("details");
+    table.text("location");
+    table.date("date").notNullable();
+    table.time('time').notNullable();
+    table.boolean("completed").defaultTo(false);
+    table.integer("creator_id").references("id").inTable("users").notNullable();
+    table.integer("invitee_id").references("id").inTable("users");
+    table.boolean("accepted").defaultTo(false);
+
+
+
+
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("appointments")
 };
