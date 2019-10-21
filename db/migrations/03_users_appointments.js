ab07caacae5a1b869e138a8add7624d6b7710c8c
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-20 17:18:19 -0700

    adjusting query for join table

--- db/migrations/03_users_appointments.js
+++ db/migrations/03_users_appointments.js
@@ -3,8 +3,8 @@ exports.up = function (knex) {
   
   return knex.schema.createTable("users_appointments", function (table) {
     table.increments();
-    table.integer("user_id").references("id").inTable("users");
-    table.integer("appointment_id").references("id").inTable("appointments");
+    table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+    table.integer("appointment_id").references("id").inTable("appointments").onDelete("CASCADE");
     table.boolean("accepted").defaultTo(false);
   })
 };
