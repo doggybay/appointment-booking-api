11ebf917e892eef2bb2f7aac6e0d50a2f67e5d0d
doggybay <julio.miranda12@gmail.com>
2019-10-21 21:02:42 -0700

    adding comments

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,11 @@
-const knex = require("../db/knex");
 const User = require('../models/User')
 
+//Locates all users and adds the appointments related based on the users_appointments table and returns all users with appointments
 exports.getAllUsers = (req, res) => {
   User.query().eager('appointments').then(users => res.json(users))
 }
 
+//Locates and returns said user with appointments related to user based on join table
 exports.getOneUser = (req, res) => {
   User.query().findById(req.params.id).eager('appointments').then(user => res.json(user))
 }
